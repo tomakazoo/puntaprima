13a4b3706472023a3dba2c7512e86350b4793d47
Tomislav Babic <tomislavb@gmail.com>
2020-01-08 01:32:55 +0100

    change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: `/gatsby-creative`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Amazing holiday`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
