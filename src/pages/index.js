20dd820a6c650c335a62b00c7c9ee100d9e4a7eb
Tomislav Babic <tomislavb@gmail.com>
2020-01-08 01:12:29 +0100

    inital commit

---
+++ src/pages/index.js
@@ -0,0 +1,227 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Scroller from "../components/scroller"
+import PortfolioModal from "../components/portfolio/modal"
+import PortfolioCarousel from "../components/portfolio/carousel"
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    Scroller.handleAnchorScroll = Scroller.handleAnchorScroll.bind(this)
+    this.state = {
+      modalShow: false,
+      modalCurrent: 0
+    }
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this);
+    this.setModal = this.setModal.bind(this);
+  }
+
+  handlePortfolioClick(index, e) {
+    e.preventDefault();
+    this.setModal(true, index);
+  }
+
+  setModal(isShown, current) {
+    this.setState({
+      modalShow: isShown,
+      modalCurrent: current
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home"/>
+        <section className="page-section bg-primary" id="about">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="text-white mt-0">We've got what you need!</h2>
+                <hr className="divider light my-4"/>
+                <p className="text-white-50 mb-4"><strong>"Linda"</strong> is a nice bright apartment, located in a quiet residential called La Entrada. This complex have 3 pools and gardens around urbanization.
+This apartment have 2 bedrooms and 2 bathrooms, large windows that provide natural light and a magnificent terrace overlooking the gardens !</p>
+                <a className="btn btn-light btn-xl js-scroll-trigger" href="#services"
+                   onClick={Scroller.handleAnchorScroll}>More Info!</a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="page-section" id="services">
+          <div className="container">
+            <h2 className="text-center mt-0">At Your Service</h2>
+            <hr className="divider my-4"/>
+            <div className="row">
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-umbrella-beach text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Weather</h3>
+                  <p className="text-muted mb-0">Perfect destination for sun lovers enjoying approximately 325 sunny days each year.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-swimmer text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Location</h3>
+                  <p className="text-muted mb-0">Its location is fantastic as just a few minutes away is the beach, also find a few meters all necessary amenities as supermarkets, pharmacy, ​​bars and restaurant serving Mediterranean and international cuisine, etc ...</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-golf-ball text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Golf</h3>
+                  <p className="text-muted mb-0"> If you want to spend a day of golf, you are just a few minutes drive from the most famous golf courses in the area as those located in Las Ramblas, Campoamor and Villamartin!</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-heart text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Place with Love</h3>
+                  <p className="text-muted mb-0">Can you imagine having breakfast on the terrace with the first rays of sun day? A perfect plan for you to start the day.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="portfolio">
+          <div className="container-fluid p-0">
+            <div className="row no-gutters">
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg" onClick={this.handlePortfolioClick.bind(this, 0)}>
+                  <Img fluid={this.props.data.images.edges[0].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Terrace
+                    </div>
+                    <div className="project-name">
+                      Garden view
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg" onClick={this.handlePortfolioClick.bind(this, 1)}>
+                  <Img fluid={this.props.data.images.edges[1].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Swimming pool
+                    </div>
+                    <div className="project-name">
+                      Palm Trees
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg" onClick={this.handlePortfolioClick.bind(this, 2)}>
+                  <Img fluid={this.props.data.images.edges[2].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                     Bathroom 
+                    </div>
+                    <div className="project-name">
+                      Bath and Shower
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="images/portfolio/fullsize/4.jpg" onClick={this.handlePortfolioClick.bind(this, 3)}>
+                  <Img fluid={this.props.data.images.edges[3].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                    Parents Room
+                    </div>
+                    <div className="project-name">
+                    Double Bed
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/5.jpg" onClick={this.handlePortfolioClick.bind(this, 4)}>
+                  <Img fluid={this.props.data.images.edges[4].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                    Kids Room
+                    </div>
+                    <div className="project-name">
+                    Two Beds
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg" onClick={this.handlePortfolioClick.bind(this, 5)}>
+                  <Img fluid={this.props.data.images.edges[5].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption p-3">
+                    <div className="project-category text-white-50">
+                      Living Room
+                    </div>
+                    <div className="project-name">
+                    View on Living Room and Kitchen
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="page-section bg-dark text-white">
+          <div className="container text-center">
+            <h2 className="mb-4">Airbnb</h2>
+            <a className="btn btn-light btn-xl" href="https://www.airbnb.com/rooms/35802883?source_impression_id=p3_1578435485_fidHFHnucxrUIz6i">Visit Now!</a>
+          </div>
+        </section>
+
+        <section className="page-section" id="contact">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="mt-0">Let's Get In Touch!</h2>
+                <hr className="divider my-4"/>
+                <p className="text-muted mb-5">Tired of running around? Want perfect relaxing vacation? Lovely beaches, excellent food, nearby restaurants, shopping centre, golf courses, and many more... </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <div>+352 (621) 542-744</div>
+              </div>
+              <div className="col-lg-4 mr-auto text-center">
+                <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                <a className="d-block" href="mailto:tomislavb@gmail.com">tomislavb@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <PortfolioModal show={this.state.modalShow} onHide={() => this.setModal(false, 0)}>
+          <PortfolioCarousel images={this.props.data.images.edges} current={this.state.modalCurrent}/>
+        </PortfolioModal>
+      </Layout>
+    )
+  }
+}
+
+
+export const imageData = graphql`
+  query {
+    images: allFile(filter: {relativePath: {glob: "portfolio/fullsize/*.jpg"}}, sort: {fields: name}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
